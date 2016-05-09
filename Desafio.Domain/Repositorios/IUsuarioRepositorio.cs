ccba40d50f0dc2ea1d2bd6a3639b97417226656d
Ivan Soares <soaresbest@gmail.com>
2016-05-09 19:06:12 -0300

    Novo repositorio especifico para o usuario

---
+++ Desafio.Domain/Repositorios/IUsuarioRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using Desafio.Domain.Entidades;
+
+namespace Desafio.Domain.Repositorios
+{
+    public interface IUsuarioRepositorio: IRepositorio
+    {
+        /*
+         * Desafio .NET Concrete Solutions
+         * Ivan Soares dos Santos
+         */
+
+        Usuario CarregarPorEMail(string eMail);
+    }
+}
