a489d8a561f31e99fb90efe7264f298b83b64765
Ivan Soares <soaresbest@gmail.com>
2016-05-09 18:40:54 -0300

    Entidades Usuario e Telefone adicionadas ao dominio

---
+++ Desafio.Domain/Entidades/Telefone.cs
@@ -0,0 +1,13 @@
+﻿namespace Desafio.Domain.Entidades
+{
+    public class Telefone
+    {
+        /*
+         * Desafio .NET Concrete Solutions
+         * Ivan Soares dos Santos
+         */
+
+        public string Numero { get; set; }
+        public string DDD { get; set; }
+    }
+}
---
+++ Desafio.Domain/Entidades/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Desafio.Domain.Entidades
+{
+    public class Usuario
+    {
+        /*
+         * Desafio .NET Concrete Solutions
+         * Ivan Soares dos Santos
+         */
+
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public string Senha { get; set; }
+        public IList<Telefone> Telefones { get; set; }
+    }
+}
