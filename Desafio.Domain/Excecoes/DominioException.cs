d045989648670d4ef703e622ae4503351c26f545
Ivan Soares <soaresbest@gmail.com>
2016-05-09 19:26:45 -0300

    Novas excecoes do dominio

---
+++ Desafio.Domain/Excecoes/DominioException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desafio.Domain.Excecoes
+{
+    public class DominioException : Exception
+    {
+        /*
+         * Desafio .NET Concrete Solutions
+         * Ivan Soares dos Santos
+         */
+
+        public DominioException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ Desafio.Domain/Excecoes/EMailExistenteException.cs
@@ -0,0 +1,20 @@
+﻿namespace Desafio.Domain.Excecoes
+{
+    public class EMailExistenteException : DominioException
+    {
+        /*
+         * Desafio .NET Concrete Solutions
+         * Ivan Soares dos Santos
+         */
+
+        public EMailExistenteException(string eMail)
+            : base(MontarMensagem(eMail))
+        {
+        }
+
+        private static string MontarMensagem(string eMail)
+        {
+            return string.Format("E-mail '{0}' já existente.", eMail);
+        }
+    }
+}
